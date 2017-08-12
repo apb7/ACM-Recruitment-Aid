cb42b1e5b238e6103ceac09c002d8192d47d0aa1
Apurv-Bajaj <apurvabajaj007@gmail.com>
2017-08-12 23:21:06 +0530

    Inital Commit

---
+++ recruit/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url, include
+#from django.contrib import admin
+from . import views
+
+app_name='recruit'
+urlpatterns = [
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^add/$', views.add_recruit, name='add'),
+    url(r'^exist/$', views.exist_recruit, name='exist'),
+
+]
