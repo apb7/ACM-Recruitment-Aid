bbf67d0c0896a37892e4315c257085c7c02ff91f
Apurv-Bajaj <apurvabajaj007@gmail.com>
2017-08-15 14:46:04 +0530

    Field added

--- recruit/models.py
+++ recruit/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Recruit(models.Model):
     name=models.CharField(max_length=200)
     student_id=models.CharField(max_length=50)
+    contact_no=models.CharField(max_length=20)
     skills=models.CharField(max_length=1000)
 
 
@@ -14,6 +15,6 @@ class Member(models.Model):
     remarks=models.CharField(max_length=1000)
     grilled_by=models.CharField(max_length=200)
     task=models.CharField(max_length=200)
-
+    
     def __str__(self):
         return self.grilled_by
